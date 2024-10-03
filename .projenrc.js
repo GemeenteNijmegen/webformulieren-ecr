2421db080924f251fedaebd136a16899af43ac39
Wieteke <149399985+WietekeNijmegen@users.noreply.github.com>
2024-10-03 10:10:00 +0200

    feat: projen upgrades

--- .projenrc.js
+++ .projenrc.js
@@ -2,6 +2,11 @@ const { GemeenteNijmegenCdkApp } = require('@gemeentenijmegen/projen-project-typ
 const project = new GemeenteNijmegenCdkApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
+  depsUpgradeOptions: {
+    workflowOptions: {
+      branches: ['main'], // No acceptance branch
+    },
+  },
   devDeps: [
     '@gemeentenijmegen/projen-project-type',
   ],
